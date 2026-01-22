167701dbc90a91a8b9f43beb89503eba844ce238
GalacticQuasar <akashdfw@gmail.com>
2026-01-22 01:57:14 -0500

    feat: hello world styling

--- django-project/app/views.py
+++ django-project/app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    # return HttpResponse("Hello, World!")
+    return render(request, 'index.html')
