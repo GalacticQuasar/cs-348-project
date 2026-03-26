e8a39fe7123fdc4750e7904c948fee8845b60ead
GalacticQuasar <akashdfw@gmail.com>
2026-03-26 18:07:32 -0400

    feat: register tables for admin access

--- django-project/app/admin.py
+++ django-project/app/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Brand, CarModel, Customer, Sale, SalesPerson
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+	list_display = ("id", "name")
+	search_fields = ("name",)
+
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+	list_display = ("id", "brand", "model_name")
+	list_filter = ("brand",)
+	search_fields = ("model_name", "brand__name")
+
+
+@admin.register(SalesPerson)
+class SalesPersonAdmin(admin.ModelAdmin):
+	list_display = ("id", "first_name", "last_name")
+	search_fields = ("first_name", "last_name")
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+	list_display = ("id", "first_name", "last_name", "email")
+	search_fields = ("first_name", "last_name", "email")
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+	list_display = ("id", "date", "salesperson", "customer", "car_model", "sale_price")
+	list_filter = ("date", "salesperson", "car_model__brand")
+	search_fields = (
+		"salesperson__first_name",
+		"salesperson__last_name",
+		"customer__first_name",
+		"customer__last_name",
+		"car_model__model_name",
+		"car_model__brand__name",
+	)
