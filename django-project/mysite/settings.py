c0cfb6ee16073b167c2b4819e9d162b8ef057057
GalacticQuasar <akashdfw@gmail.com>
2026-04-28 17:53:58 -0400

    add: atomic requests setting and migration

--- django-project/mysite/settings.py
+++ django-project/mysite/settings.py
@@ -77,6 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ATOMIC_REQUESTS': True,
+        # SQLite uses SERIALIZABLE by default
     }
 }
 
